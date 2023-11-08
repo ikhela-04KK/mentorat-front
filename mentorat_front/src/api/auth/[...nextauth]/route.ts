ecb37cfeb84f882d4cffed55daff47dfb0346060
Koffi Kan Marc-Ephrem <102194271+ikhela-04KK@users.noreply.github.com>
2023-11-08 18:11:06 +0000

    Create route.ts

---
+++ mentorat_front/src/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+// import {NextAuthOptions} from "next*auth"; 
+// import CredentialsProvider from 'next-auth/providers/credentials'; 
+
+
+// export const authOption: NextAuthOptions = {
+//     providers:[
+//         CredentialsProvider({
+//             name:"Credentials", 
+//             credentials:{
+//                 username:{
+//                     label:"Username", 
+//                     type:"text",
+//                     placeholder:
+//                 }
+//             }
+//         })
+//     ]
+// }
