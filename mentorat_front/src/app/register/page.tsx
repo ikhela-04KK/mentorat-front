9f005f8fccb1b1737d7f2593c7770bcd85c0060b
Koffi Kan Marc-Ephrem <102194271+ikhela-04KK@users.noreply.github.com>
2023-11-09 07:19:20 +0000

    Update page.tsx

--- mentorat_front/src/app/register/page.tsx
+++ mentorat_front/src/app/register/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 // import Login from "@/features/users/login/login";
 import SignIn from "@/features/users/signIn/signIn";
 
-
+// forUsername
 export default function Home() {
   return (
     <>
